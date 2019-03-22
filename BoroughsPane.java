7d1203810fb32cc42a933546702cfe35ecfbf347
Todorov, Martin (k1890496) <martin.todorov@kcl.ac.uk>
2019-03-22 13:57:50 +0000

    Comments and formatting

--- BoroughsPane.java
+++ BoroughsPane.java
@@ -1,47 +1,27 @@
-//TODO: sort out imports
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-import javafx.geometry.Insets;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import org.controlsfx.control.RangeSlider;
-import com.jfoenix.controls.JFXButton;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.ArrayList;
-import javafx.fxml.*;
-import java.net.URL;
-import java.io.File;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.util.Duration;
-
-import javafx.scene.control.ScrollPane;
-import javafx.application.Application;
+import javafx.scene.input.MouseEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.geometry.*;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
-import java.util.Queue;
-import java.util.LinkedList;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import java.util.ArrayList; 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.scene.input.*;
-import java.util.Set;
-import java.util.Iterator;
-
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import com.jfoenix.controls.JFXButton;
 
 /**
- * TODO: comment here
+ * This class represents the pane that shows a geographically-accurate representation
+ * of all the boroughs in London. It consists of a button for each neighbourhood, all of 
+ * which are heat mapped - the darker the color, the more available properties there are
+ * Upon clicking a button a new window pops up showing a list of all properties in that
+ * borough.
  * 
  * @author Alexis Dumon, Federico Barbero, Martin Todorov and Maximilian Ghazanfar
  * @version 1.0
@@ -49,37 +29,50 @@ import javafx.scene.image.ImageView;
 public class BoroughsPane extends ExtendedBorderPane
 {    
     // decalre static label prefixes for easier localization of the app:
-    private static final String LOW_PREFIX, HIGH_PREFIX, PROP_COUNT_PREFIX;
+    private static final String LOW_PREFIX, HIGH_PREFIX, PROP_COUNT_PREFIX,
+                                POPUP_TITLE_PREFIX;
     static {
-         LOW_PREFIX = "Low";
-         HIGH_PREFIX = "High";
-         PROP_COUNT_PREFIX = "Property count";
+        LOW_PREFIX = "Low";
+        HIGH_PREFIX = "High";
+        PROP_COUNT_PREFIX = "Property count";
+        POPUP_TITLE_PREFIX = "Showing all properties in: ";
     }
     
+    // the window that pops up when one of the boroughs is clicked
     private Stage popUpWin;
     
+    // all of the borough buttons
     private ArrayList<BoroughButton> boroughButtons;
-
-    private AnchorPane buttonWrap;
     
     /**
      * Constructor for objects of class BoroughsPane
      */
     public BoroughsPane(AirbnbDataMap data)
     {
+        // pass the data along to the super constructor
         super(data);
         
+        // init the array to store all buttons
         boroughButtons = new ArrayList<BoroughButton>();
         
+        // get the names of all boroughs with respect to geographic
+        // locations in an array list
         ArrayList<String> boroughs = data.getGeoOrderedBoroughs();
         
+        // create a button for each borough
         for(String boroughName : boroughs) {
+            // the button text will be the first for letters of the borough
+            // name, in upper-case only
             String btnText = boroughName.substring(0, 4).toUpperCase();
             
+            // init button (BoroughButton extends JFXButton and adds a private
+            // field to store the borough represented by the button)
             BoroughButton btn = new BoroughButton(btnText, boroughName);
             
             btn.setOnAction(this::setSelectedBorough);
             
+            // css scales the button on hover, thus it needs to be bringed to front
+            // so that it is above all other button
             btn.addEventHandler(MouseEvent.MOUSE_ENTERED,
                 new EventHandler<MouseEvent>() {
                   @Override
@@ -91,14 +84,20 @@ public class BoroughsPane extends ExtendedBorderPane
             btn.setPrefHeight(75.0);
             btn.setPrefWidth(70.0);
             btn.getStyleClass().add("boroughButton");
+            
+            // add the button to the array list for future reference
             boroughButtons.add(btn);
         }
         
-        buttonWrap = new AnchorPane();
+        // wrap all of the buttons in an anchor pane
+        AnchorPane buttonWrap = new AnchorPane();
         buttonWrap.getChildren().addAll(boroughButtons);
         
+        // position buttons in a 'beehive' pattern
         positionButtons();
         
+        // create the heat map legend:
+
             Label lowLabel = new Label(LOW_PREFIX);
             lowLabel.getStyleClass().add("smallLabelBoroughsPane");
             Label propCount = new Label(PROP_COUNT_PREFIX);
@@ -111,17 +110,24 @@ public class BoroughsPane extends ExtendedBorderPane
             heatMapLegend.setMinHeight(5);
             heatMapLegend.setPrefHeight(5);
             heatMapLegend.getStyleClass().add("heatMapLegend");
-            
+        
+        // the heat map legend is stored in a BorderPane
         BorderPane heatMapLegendWrap = new BorderPane(propCount, null, highLabel, heatMapLegend, lowLabel);
         heatMapLegendWrap.setPrefWidth(250);
         heatMapLegendWrap.setMaxWidth(150);
         
+        // the buttons go in the center
         setCenter(buttonWrap);
+        // the heat map legend goes in the footer
         setBottom(heatMapLegendWrap);
+        // center
         setAlignment(heatMapLegendWrap, Pos.TOP_CENTER);
         
+        // init the stage for the popUpWindow so that
+        // a new window is not opened every time
         popUpWin = new Stage();
         
+        // dimensions:
         setMinHeight(480);
         setMaxHeight(480);
         setPrefHeight(480);
@@ -129,18 +135,20 @@ public class BoroughsPane extends ExtendedBorderPane
 
     /**
      * Updates the heat map of boroughs based on availability of properties
-     * in the currently selected price range.
-     * 
-     * TODO: implement
+     * in the currently selected price range
      */
     protected void updateView()
     {
         int propertyCount = 0;
+        // iterate through all buttons
         for(BoroughButton btn : boroughButtons) {
+            // get the count of properties for a button
             propertyCount = data.getWithinPriceRange(btn.getBoroughOfButton(), minPrice, maxPrice).size();
+            
+            // disable the button if there are no properties for its borough
             if(propertyCount == 0) {
                 btn.setDisable(true);
-            } else {
+            } else { // heat map by setting the background based on the property count in the button's borough
                 if(propertyCount < 700) {
                     if(propertyCount < 500) {
                         if(propertyCount < 300) {
@@ -160,40 +168,55 @@ public class BoroughsPane extends ExtendedBorderPane
                         btn.setDisable(false);
                     }
                 } else {
-                btn.setBackground((new Background(new BackgroundFill(Color.web("ce4a4e"), CornerRadii.EMPTY, Insets.EMPTY))));
-                btn.setDisable(false);
+                    btn.setBackground((new Background(new BackgroundFill(Color.web("ce4a4e"), CornerRadii.EMPTY, Insets.EMPTY))));
+                    btn.setDisable(false);
                 }
             }
         }
         
+        // close the pop-up window if its open, because the price range is now changed,
+        // thus new properties should be displayed
         popUpWin.close();
     }
     
     /**
-     * Alrets the class controlling the other panes that
-     * the selected borough has changed so that the other panes can
-     * be updated accordingly.
-     * 
-     * @param name the name of the newly selected borough
+     * Handles the functionality of every BoroughButton
      */
     private void setSelectedBorough(ActionEvent e) 
     {   
+        // get the button
         BoroughButton btn = (BoroughButton) e.getSource();
+
+        // get the name of borough it is representing
+        // (from a private field so that localization is seamless)
+        // and set it as the selected borough in the HashMap
         data.setCurrentBorough(btn.getBoroughOfButton());
         
+        // get the list of properties in that borough
         PropertyListWindow propList = new PropertyListWindow(data);
         Pane newWinRoot = propList.getView();
         
+        // put the list in a new scene
         Scene newScene = new Scene(newWinRoot);
         newScene.getStylesheets().add("propertyListStyle.css");
+
+        // put the scene in the new window
         popUpWin.setScene(newScene);
         
-        popUpWin.setTitle("Properties in: " + btn.getBoroughOfButton());
+        // set title and show
+        popUpWin.setTitle(POPUP_TITLE_PREFIX + btn.getBoroughOfButton());
         popUpWin.show();
     }
 
+    /**
+     * This method is responsible for positioning all buttons representing a borough
+     * in a 'beehive' pattern. The logic behind the method uses the concept
+     * of a 'sparse' matrix which defines where the buttons should be placed
+     */
     private void positionButtons()
     {
+        // this grid indicates where buttons should be placed
+        // true means place a button here and false means dont
         boolean grid[][] = new boolean[][] {
             {false,  false,  false,  false,  false,  false,  false,  true,   false,  false,  false,  false,  false,  false},
             {false,  false,  false,  false,  true,   false,  true,   false,  true,   false,  false,  false,  false,  false},
@@ -204,12 +227,17 @@ public class BoroughsPane extends ExtendedBorderPane
             {false,  false,  false,  true,   false,  true,   false,  true,   false,  true,   false,  false,  false,  false},
         };
 
+        // keep track which button we are positioning
         int btnIndex = 0;
 
+        // iterate through grid...        
         for(int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if(grid[i][j]) {
+                    // offsetX by half the buttons width + a constant 80px
+                    // to center them based on the window
                     boroughButtons.get(btnIndex).setLayoutX(j*37 + 80);
+                    // offset Y by enough to be a beehive pattern
                     boroughButtons.get(btnIndex).setLayoutY(i*59);
                     btnIndex ++;
                 }
