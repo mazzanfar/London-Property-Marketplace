d67992225fccf8ed6f233e7c11b56799f7e861ff
Barbero, Federico (k1888740) <federico.barbero@kcl.ac.uk>
2019-03-28 16:55:45 +0000

    Statistics merge
    Full statistics merge -- misses unit test

---
+++ Statistics.java
@@ -0,0 +1,375 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Collection;
+import java.lang.Math;
+
+import java.net.URL;
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+/**
+ * This class is used to compute the statistics needed for the statisticsPane
+ * class. The calculations are in a separate class to follow the standard design
+ * pattern of separating data manipulation and data presentation.
+ * 
+ * @author Alexis Dumon, Federico Barbero, Martin Todorov and Maximilian
+ *         Ghazanfar
+ * @version 2.0
+ */
+
+public class Statistics {
+    private ArrayList<AirbnbListing> dataArray;
+    private AirbnbDataMap dataMap;
+    private TFLStationsData tflStations;
+
+    // close means within 200m range from station
+    public static final double closeDistance = 200;
+
+    /**
+     * Constructor for Statistics
+     */
+    public Statistics() {
+        tflStations = new TFLStationsData();
+        update();
+    }
+
+    /**
+     * Update (load) the data imported from the csv files
+     */
+    public void update() {
+        dataMap = new AirbnbDataMap();
+        dataArray = new ArrayList<AirbnbListing>();
+
+        for (ArrayList arr : dataMap.values()) {
+            dataArray.addAll(arr);
+        }
+
+    }
+
+    /**
+     * Get the average reviews from all of the properties
+     * 
+     * @return double The average review from all of the properties
+     */
+    public double getAverageReviews() {
+        return dataArray.stream().mapToInt(e -> e.getNumberOfReviews()).average().orElse(0);
+    }
+
+    /**
+     * Get the total number of properties
+     * 
+     * @return int The total number of properties
+     */
+    public int getTotalProperties() {
+        return dataArray.size();
+    }
+
+    /**
+     * Get the number of Home and Apartments (everything which is not a private
+     * room)
+     * 
+     * @return double The number of Home and Apartments
+     */
+    public double getHomeAndApartments() {
+        return dataArray.stream().filter(e -> e.getRoom_type() != "Private room").count();
+    }
+
+    /**
+     * Get the most expensive house (minimum nights * price)
+     * 
+     * @return int The most expensive house (ordered by minimum nights * price)
+     */
+    public int getMostExpensive() {
+        return dataArray.stream().mapToInt(e -> {
+            return e.getMinimumNights() * e.getPrice();
+        }).max().orElse(0);
+    }
+
+    /**
+     * Get the number of properties within a certain price range
+     * 
+     * @param int Minimum price in the range
+     * @param int Maximum price in the range
+     * 
+     * @return double The number of properties between the min and max price range
+     */
+    public double getPropertiesWithinPriceRange(int min, int max) {
+        return dataArray.stream().mapToInt(e -> {
+            return e.getMinimumNights() * e.getPrice();
+        }).filter(e -> e >= min && e <= max).count();
+    }
+
+    /**
+     * Get the number of properties within a certain price range in a specific
+     * borough
+     * 
+     * @param int Minimum price in the range
+     * @param int Maximum price in the range
+     * 
+     * @return double The number of properties between the min and max price range
+     *         in a specific borough
+     */
+    public double getPropertiesWithinPriceRange(int min, int max, String borough) {
+        return getSpecificBorough(borough).stream().mapToInt(e -> {
+            return e.getMinimumNights() * e.getPrice();
+        }).filter(e -> e >= min && e <= max).count();
+    }
+
+    /**
+     * Get the borough which has the most houses for a specific price range
+     * 
+     * @param int Minimum price in the range
+     * @param int Maximum price in the range
+     * 
+     * @return String The name of the suggested borough (borough which has the
+     *         highest number of houses within a price range)
+     */
+    public String getSuggestedBorough(int min, int max) {
+        Set<String> boroughs = dataMap.getDistinctBoroughsSet();
+        double maxCount = 0;
+        String maxBorough = "";
+
+        for (String borough : boroughs) {
+            double propertyCount = getPropertiesWithinPriceRange(min, max, borough);
+            if (propertyCount > maxCount) {
+                maxCount = propertyCount;
+                maxBorough = borough;
+            }
+        }
+
+        return maxBorough;
+    }
+
+    /**
+     * Get number of properties close (within 200m) of a station, distance is
+     * computed via the Haversine method from the geo-cordinates of the property and
+     * the station
+     * 
+     * @return int Number of properties which are close (within 200m) to an official
+     *         TFL station
+     */
+    public int getPropertiesCloseToStation() {
+        int count = 0;
+
+        for (AirbnbListing listing : dataArray) {
+            for (TFLStation station : tflStations.getStations()) {
+                double distance = getDistanceBetweenGeoCoordinates(listing.getLatitude(), station.getLatitude(),
+                        listing.getLongitude(), station.getLongitude());
+                if (distance <= closeDistance)
+                    count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    /**
+     * Get the borough which has the highest number of properties which are close
+     * (within 200m) of an official TFL station, distance is computed via the
+     * Haversine method from the geo-cordinates of the property and the station
+     * 
+     * @return String The name of the borough which has the highest number of
+     *         properties which are close (within 200m) of a TFL station
+     */
+
+    public String getMostServedBorough() {
+        Set<String> boroughs = dataMap.getDistinctBoroughsSet();
+        int maxCount = 0;
+        String maxBorough = "";
+
+        for (String borough : boroughs) {
+            int count = 0;
+            for (AirbnbListing listing : getSpecificBorough(borough)) {
+                for (TFLStation station : tflStations.getStations()) {
+                    double distance = getDistanceBetweenGeoCoordinates(listing.getLatitude(), station.getLatitude(),
+                            listing.getLongitude(), station.getLongitude());
+                    if (distance <= closeDistance)
+                        count += 1;
+                }
+
+                if (count > maxCount) {
+                    maxCount = count;
+                    maxBorough = borough;
+                }
+            }
+        }
+
+        return maxBorough;
+    }
+
+    /**
+     * https://stackoverflow.com/questions/3694380/calculating-distance-between-two-points-using-latitude-longitude
+     * this method is a modification from this stackoverflow thread
+     * 
+     * Calculate distance between two points in latitude and longitude. Uses
+     * Haversine method as its base.
+     * 
+     * @param double Latitude of first property
+     * @param double Latitude of second property
+     * @param double Longitude of first property
+     * @param double Longitude of second property
+     * 
+     * @returns Distance in meters between the two geo-coordinates given
+     */
+    private static double getDistanceBetweenGeoCoordinates(double lat1, double lat2, double lon1, double lon2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        return distance;
+    }
+
+    /**
+     * Get properties in the currently selected borough
+     * 
+     * @return ArrayList<AirbnListing> List of AirbnbListing objects of the current
+     *         selected borough
+     */
+    private ArrayList<AirbnbListing> getCurrentBorough() {
+        return dataMap.get(dataMap.getCurrentBorough());
+    }
+
+    /**
+     * Get properties in a specific borough
+     * 
+     * @return ArrayList<AirbnListing> List of AirbnbListing objects of a specific
+     *         borough
+     */
+    private ArrayList<AirbnbListing> getSpecificBorough(String borough) {
+        return dataMap.get(borough);
+    }
+
+    /**
+     * This class is responsible of pulling data from the stations CSV file
+     */
+    public class TFLStationsData {
+        private ArrayList<TFLStation> stations;
+
+        /**
+         * Constructor for TFLStationsData
+         */
+        public TFLStationsData() {
+            stations = new ArrayList<TFLStation>();
+            load();
+        }
+
+        /**
+         * Load data from the stations CSV file and update the stations field
+         */
+        public void load() {
+            System.out.print("Begin loading TFL Stations london dataset...");
+            try {
+                URL url = getClass().getResource("stations-london.csv");
+                CSVReader reader = new CSVReader(new FileReader(new File(url.toURI()).getAbsolutePath()));
+                String[] line;
+                // skip the first row (column headers)
+                reader.readNext();
+                while ((line = reader.readNext()) != null) {
+                    String station = line[0];
+                    String OSX = line[1];
+                    String OSY = line[2];
+                    double latitude = convertDouble(line[3]);
+                    double longitude = convertDouble(line[4]);
+                    String zone = line[5];
+                    String postcode = line[6];
+
+                    TFLStation tflstation = new TFLStation(station, OSX, OSY, latitude, longitude, zone, postcode);
+
+                    stations.add(tflstation);
+                }
+
+            } catch (IOException | URISyntaxException e) {
+                System.out.println("Failure! Something went wrong");
+                e.printStackTrace();
+            }
+            System.out.println("Success! Number of loaded records: " + stations.size());
+        }
+
+        /**
+         * Return the stations list
+         * 
+         * @return ArrayList<TFLStation> The list of stations
+         */
+        public ArrayList<TFLStation> getStations() {
+            return stations;
+        }
+
+        /**
+         * Convert string to double
+         * 
+         * @param String string to be converted to double
+         * 
+         * @return Double the double converted from the string, -1.0 if the string could
+         *         not be converted
+         */
+        private Double convertDouble(String doubleString) {
+            if (doubleString != null && !doubleString.trim().equals("")) {
+                return Double.parseDouble(doubleString);
+            }
+            return -1.0;
+        }
+    }
+
+    /**
+     * This class is a helper class for TFLStationsData, it structures the object
+     * which is then inserted in the stations list
+     */
+    public class TFLStation {
+        private String station;
+        private String OSX;
+        private String OSY;
+        private double latitude;
+        private double longitude;
+        private String zone;
+        private String postcode;
+
+        /**
+         * Constructor for TFLStation
+         * 
+         * @param String The station
+         * @param String OSX
+         * @param String OSY
+         * @param double The latitude
+         * @param double The longitude
+         * @param String The London zone
+         * @param String The postcode
+         */
+        public TFLStation(String station, String OSX, String OSY, double latitude, double longitude, String zone,
+                String postcode) {
+            this.station = station;
+            this.OSX = OSX;
+            this.OSY = OSY;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.zone = zone;
+            this.postcode = postcode;
+        }
+
+        /**
+         * The latitude of the station
+         * 
+         * @return double The latitude of the station
+         */
+        public double getLatitude() {
+            return latitude;
+        }
+
+        /**
+         * The longitude of the station
+         * 
+         * @return double The longitude of the station
+         */
+        public double getLongitude() {
+            return longitude;
+        }
+    }
+}
--- StatisticsPane.java
+++ StatisticsPane.java
@@ -1,37 +1,243 @@
+
 //TODO: sort imports
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.animation.*;
+import org.controlsfx.control.RangeSlider;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import javafx.util.Duration;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import java.text.DecimalFormat;
+import javafx.geometry.Pos;
 
 /**
- * TODO: comment here
+ * This class shows statistics for all properties in the currently selected
+ * borough and price range.
  * 
- * @author Alexis Dumon, Federico Barbero, Martin Todorov and Maximilian Ghazanfar
- * @version 1.0
+ * @author Alexis Dumon, Federico Barbero, Martin Todorov and Maximilian
+ *         Ghazanfar
+ * @version 2.0
  */
-public class StatisticsPane extends ExtendedBorderPane
-{
+public class StatisticsPane extends ExtendedBorderPane {
+    private Statistics statistics;
+
+    private ArrayList<Label> labels;
+    private int counter;
+    private int loadingCounter;
+
+    // format to two decimal places
+    private DecimalFormat df;
+
+    private Timeline loadingAnimation;
+    private Timeline statisticsAnimation;
+
+    // TODO: MOVE THIS
+    public static final int PANE_WIDTH = 700;
+    public static final int PANE_HEIGHT = 500;
+
     /**
      * Constructor for objects of class StatisticsPane
      */
-    public StatisticsPane(AirbnbDataMap data)
-    {
+    public StatisticsPane(AirbnbDataMap data) {
         super(data);
-        
-        //placeholder image:
-        Image image = new Image(getClass().getResourceAsStream("img/3.png"));
-        Label label1 = new Label();
-        label1.setGraphic(new ImageView(image));
-        
-        setCenter(label1);
+
+        statistics = new Statistics();
+
+        labels = new ArrayList<Label>();
+
+        // set dimensions
+        setPrefHeight(500);
+        setMinHeight(500);
+        setMaxHeight(500);
+        setPrefWidth(700);
+        setMinWidth(700);
+        setMaxWidth(700);
+
+        loadingAnimation = new Timeline(new KeyFrame(Duration.millis(500), e -> {
+            loadingAnimation();
+        }));
+
+        statisticsAnimation = new Timeline(new KeyFrame(Duration.seconds(3), e -> {
+            statisticsAnimation();
+        }));
+
+        loadingAnimation.setCycleCount(Animation.INDEFINITE);
+        statisticsAnimation.setCycleCount(Animation.INDEFINITE);
     }
-    
+
     /**
-     * Update the pane to show properties within the new price range
+     * This method is called every time the scroll bar is called. 
+     * It is responsible for drawing the animations on the GUI.
      */
-    protected void updateView()
-    {
-        //TODO: implement...
+    public void updateView() {
+        statisticsAnimation.stop();
+        
+        // Running the computations on a new thread allows the program to still function and not freeze while it is doing the computations. 
+        // Since the computations are rather long, this is a very nice workaround.
+        Thread sideCompute = new Thread(() -> {
+            // start loading animation when calling the statistics methods 
+            loadingAnimation.play();
+            createLabel("Average reviews: " + format(statistics.getAverageReviews(), 2));
+            createLabel("Total properties: " + format(statistics.getTotalProperties(), 0));
+            createLabel("Home and Apartments: " + format(statistics.getHomeAndApartments(), 0));
+            createLabel("Most expensive: " + format(statistics.getMostExpensive(), 0));
+            createLabel(
+                    "Properties within price range: " + format(statistics.getPropertiesWithinPriceRange(minPrice, maxPrice), 0));
+            createLabel("Suggested borough for price range: " + statistics.getSuggestedBorough(minPrice, maxPrice));
+            createLabel("Number of properties close to transportation: "
+                    + format(statistics.getPropertiesCloseToStation(), 0));
+            createLabel("Most served borough (transportation):" + statistics.getMostServedBorough());
+
+            // stop animation and start displaying the statistics
+            loadingAnimation.stop();
+
+            statisticsAnimation.play();
+        });
+
+        sideCompute.start();
+    }
+
+    /**
+     * Create a new label and add it to the labels list
+     * 
+     * @param String The text which should be displayed by the label
+     * @return Label the created label
+     */
+    private Label createLabel(String text) {
+        Label label = new Label(text);
+        label.getStyleClass().add("statisticslabel");
+        label.setMinWidth(PANE_WIDTH / 2);
+        label.setMinHeight(PANE_HEIGHT / 2);
+        labels.add(label);
+        return label;
+    }
+
+    /**
+     * Helper method for statistics timeline animation
+     */
+    private void statisticsAnimation() {
+        GridPane gridPane = new GridPane();
+
+        gridPane.add(addLabel(), 0, 0);
+        gridPane.add(addLabel(), 1, 0);
+        gridPane.add(addLabel(), 0, 1);
+        gridPane.add(addLabel(), 1, 1);
+        counterOffset();
+        setCenter(gridPane);
+    }
+
+    /**
+     * Helper method for loading timeline animation
+     */
+    private void loadingAnimation() {
+        Label welcomeLabel1 = new Label(loadingString());
+        welcomeLabel1.setWrapText(true);
+        welcomeLabel1.getStyleClass().add("welcomeLabel1");
+
+        Separator separator = new Separator();
+
+        Label welcomeLabel2 = new Label("Loading, please wait.");
+        welcomeLabel2.getStyleClass().add("welcomeLabel2");
+
+        // put labels in a vertical box, center and set dimensions
+        VBox welcomeWrap = new VBox(welcomeLabel1, separator, welcomeLabel2);
+        welcomeWrap.setMaxWidth(400);
+        welcomeWrap.setPrefHeight(500);
+        welcomeWrap.setAlignment(Pos.CENTER);
+
+        // set the center of this class to be the VBox created above
+        setCenter(welcomeWrap);
+    }
+
+    /**
+     * Create the animated loading string to display in the loading animation
+     * depending on the loadingCounter
+     * 
+     * @return String The dynamic string to display on the loading screen
+     */
+    private String loadingString() {
+        String loadString = "Loading";
+        loadingCounter++;
+
+        // append "" "." ".." "..." depending on loadingCounter (mod 4)
+        for (int i = 0; i < loadingCounter % 4; i++) {
+            loadString += ".";
+        }
+
+        return loadString;
+    }
+
+    /**
+     * Add label to the statistics animated panel
+     * 
+     * @return Label The label to display on the statistics animated panel
+     */
+    private Label addLabel() {
+        counter++;
+        // return the label in the array which is in rotation depending on the counter
+        // mod the cardinality of the labels
+        return labels.get(counter % labels.size());
+    }
+
+    /**
+     * Offset counter by -3
+     */
+    private void counterOffset() {
+        // we want to offset the counter by 3 to create a "snake" animation with the
+        // statistics display
+        counter -= 3;
+    }
+
+    /**
+     * Format the string to be displayed on the statistics screen.
+     * 
+     * @param Object A number-type object which can be casted to int or Double
+     * @param int    The number of decimal places to show for the decimal number
+     * 
+     * @return String The formatted results
+     */
+    private String format(Object number, int decimalPlaces) {
+        try {
+            // if decimalPlaces is 0  we want to try to cast number to 
+            // an int. 
+            if (decimalPlaces == 0) {
+                try {
+                    int castToInt = (int) number;
+                    return Integer.toString(castToInt);
+                } catch (ClassCastException e) {
+                    // assume number is of type Double
+                    Double doubleTest = (Double) number;
+                    int castToInt = doubleTest.intValue();
+                    return Integer.toString(castToInt);
+                } catch (Exception e) {
+                    // was not able to cast to int
+                    System.out.println("Exception in trying to cast class " + number.getClass() + " to int.");
+                    return "";
+                }
+            }
+
+            String format = ".";
+            // insert number of decimal places inside the format for 
+            // DecimalFormat constructor
+            for (int i = 0; i < decimalPlaces; i++) {
+                format += "#";
+            }
+
+            df = new DecimalFormat(format);
+            return df.format(number);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Illegal argument, not an instance of number.");
+            return "";
+        }
     }
 }
